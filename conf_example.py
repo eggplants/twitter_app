bae37b6764187bcad0c78245a0b0c397927bdbe1
eggplants <w10776e8w@yahoo.co.jp>
2019-05-20 06:55:26 +0900

    add an example of confif file

---
+++ conf_example.py
@@ -0,0 +1,14 @@
+#twitter consumer key
+CK = 'xx'
+#twitter consumer sec
+CS = 'xx'
+#twitter access token
+t_AT = 'xx'
+#twitter access sec
+AS = 'xx'
+#line access token
+l_AT= 'xx'
+#line talkroom id
+l_id = "xx"
+#terget user id("screen_name")
+t_user_id="xx"
